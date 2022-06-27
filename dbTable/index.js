b0f4cc11b2bd821d9b0a7fbc1b04771e63c5c3d3
Chi Hoe Low <chihoe.low@wizvision.local>
2022-06-27 17:36:14 +0800

    mysql change 3

--- dbTable/index.js
+++ dbTable/index.js
@@ -5,7 +5,7 @@ const { Client } = pg;
 const config = dotenv.config()
 console.log('config>>>>>>', config)
 const client = new Client({
-    host: Number(config.parsed.DB_HOST),
+    host: config.parsed.DB_HOST,
     user: config.parsed.DB_USERNAME,
     database: config.parsed.DB_DATABASE,
     password: config.parsed.DB_PASSWORD,
